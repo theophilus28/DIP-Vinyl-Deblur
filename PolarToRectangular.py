be304e91a210547bda53e644ff319eb316538ae5
theophilus28 <111762754+theophilus28@users.noreply.github.com>
2024-12-08 15:56:39 -0500

    using stacked rgb

--- PolarToRectangular.py
+++ PolarToRectangular.py
@@ -1,4 +1,5 @@
 import skimage as sk
+from skimage.util import img_as_float
 import numpy as np
 import cv2
 from scipy.fft import fft2
@@ -6,13 +7,20 @@ from scipy import ndimage
 import matplotlib.pyplot as plt
 
 ima = cv2.imread("images/Smeared_LP_Digital_Image_Processing_FA24.png")
-center = [407,658]
+center = [658,407]
 radius =380
-ima = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
-skima = sk.transform.warp_polar(ima, center=center, radius=radius)
-fourskima = fft2(skima)
-wienerima = sk.restoration.unsupervised_wiener(skima, fourskima)
-
+plt.imshow(ima)
+plt.show()
+skima = sk.transform.warp_polar(ima, center=center, radius=radius, scaling='linear', channel_axis=-1)
+plt.imshow(skima)
+plt.show()
+psf = np.ones((5, 5)) / 25
+wienerima1 = sk.restoration.wiener(skima[:,:,0], psf, .2)
+wienerima2 = sk.restoration.wiener(skima[:,:,1], psf, .2)
+wienerima3 = sk.restoration.wiener(skima[:,:,2], psf, .2)
+wienerima = np.stack((wienerima1, wienerima2, wienerima3), axis= 2)
+plt.imshow(wienerima)
+plt.show()
 
 def polar_linear(imgin, o=None, r=None, output=None, order=1, cont=0):
     img = imgin.transpose()
@@ -31,6 +39,6 @@ def polar_linear(imgin, o=None, r=None, output=None, order=1, cont=0):
     ndimage.map_coordinates(img, (rs, ts), order=order, output=output)
     return output
 
-restoima = polar_linear(wienerima[0], r=radius)
+restoima = polar_linear(wienerima, r=radius)
 plt.imshow(restoima)
 plt.show()
